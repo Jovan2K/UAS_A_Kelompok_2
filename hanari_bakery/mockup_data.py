a625dbe89713d99ad68bd9f92a88a3e9cc38a9c7
Jovan2K <112842377+Jovan2K@users.noreply.github.com>
2025-06-24 20:49:25 +0700

    perbaikan mockup

--- hanari_bakery/mockup_data.py
+++ hanari_bakery/mockup_data.py
@@ -8,56 +8,80 @@ from data import produk_list
 #mockup masbro 
 produk_list.append(
     RotiManis(
-        nama="Roti Manis Coklat",
+        nama="Roti Manis Susu",
         kode="RM001",
-        bahan_baku={"Tepung": "500g", "Gula": "100g", "Coklat": "150g", "Ragi": "5g"},
-        biaya_produksi=3000,
-        jumlah_produksi=100,
-        harga_jual=5000
-    )
-)
-
-produk_list.append(
-    ButterCookies(
-        nama="Butter Cookies Vanilla",
-        kode="BC001",
-        bahan_baku={"Tepung": "300g", "Butter": "200g", "Vanilla": "50g"},
-        biaya_produksi=2500,
-        jumlah_produksi=80,
+        bahan_baku={
+            "Tepung Cakra": "400 ",
+            "Tepung Segitiga": "100 ",
+            "Ragi Instan": "10 ",
+            "Gula Pasir": "100 ",
+            "Kuning Telur": "3 ",
+            "Susu Cair": "250",
+            "Mentega": "110 ",
+            "Garam": "½"
+        },
+        biaya_produksi=2000,
+        jumlah_produksi=20,
         harga_jual=4000
     )
 )
 
 produk_list.append(
-    Croissant(
-        nama="Croissant Keju",
+    ButterCookies(
+        nama="Croissant",
         kode="CR001",
-        bahan_baku={"Tepung": "600", "Butter": "300", "Keju": "100"},
-        biaya_produksi=4500,
-        jumlah_produksi=60,
+        bahan_baku={
+            "Tepung Terigu": "525 ",
+            "Air": "250",
+            "Susu Cair": "125",
+            "Gula Pasir": "62 ",
+            "Ragi Instan": "8 ",
+            "Garam": "¼",
+            "Mentega Tawar": "315 ",
+            "Kuning Telur": "1 "
+        },
+        biaya_produksi=5000,
+        jumlah_produksi=12,
         harga_jual=7000
     )
 )
 
 produk_list.append(
-    KueKering(
-        nama="Kue Kering Kacang",
-        kode="KK001",
-        bahan_baku={"Tepung": "400", "Kacang": "200", "Gula": "100"},
-        biaya_produksi=2000,
-        jumlah_produksi=120,
+    Croissant(
+        nama="Butter Cookies",
+        kode="BC001",
+        bahan_baku={
+            "Mentega Tawar": "100 ",
+            "Gula Halus": "55 ",
+            "Kuning Telur": "20 ",
+            "Vanilla Extract": "½",
+            "Tepung Serbaguna": "125 ",
+            "Garam": "¼",
+            "Chocco Chips": "100 "
+        },
+        biaya_produksi=2200,
+        jumlah_produksi=15,
         harga_jual=3500
     )
 )
 
 produk_list.append(
     Muffin(
-        nama="Muffin Coklat",
+        nama="Muffin",
         kode="MF001",
-        bahan_baku={"Tepung": "350", "Coklat": "150", "Telur": "2"},
-        biaya_produksi=3200,
-        jumlah_produksi=90,
-        harga_jual=5500
+        bahan_baku={
+            "Tepung Terigu": "250 ",
+            "Gula Pasir": "150 ",
+            "Garam": "½",
+            "Baking Powder": "2",
+            "Susu Cair": "150",
+            "Telur Ayam": "2 ",
+            "Margarin Cair": "150 ",
+            "Vanili": "1"
+        },
+        biaya_produksi=2100,
+        jumlah_produksi=15,
+        harga_jual=3000
     )
 )
 
